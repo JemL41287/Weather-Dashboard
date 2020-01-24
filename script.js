c32bf4f1db91ae4db7d6617374d554f06f1bfde4
Jennifer Mullin <jennifer.lindsey.mullin@gmail.com>
2020-01-24 16:37:15 -0700

    created functions to search city, append city to search history and start to pull current donitions but not working yet

--- script.js
+++ script.js
@@ -5,10 +5,97 @@ $(document).ready(function() {
 
     getSearchHistory();
 
+    $("search-button").on("click", function(event) {
+
+        event.preventDefault();
+
+        var searchedCity = $("city-input").val();
+
+        if (!searchedCity) {
+            return null;
+        }
+
+
+        getCurrentConditions(searchedCity)
+        .then(function(currentConditions) {
+
+            appendCurrentConditions(currentConditions);
+        })
+        .then(function() {
+
+            saveCityToSearchHistory(searchedCity);
+            getSearchHistory();
+        })
+        .then(function() {
+
+            getFiveDayForecast(searchedCity)
+            .then(function(forecast) {
+
+                appendFiveDayForecast(forecast);
+            });
+        });
+
+
+    });
+
+    function getCurrentConditions(city) {
+
+        var queryURL = "https://openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+        return $.ajax({
+            url: queryURL,
+            method: "GET"
+        });
+    }
+
+    function appendCurrentConditions(currentConditions) {
+
+        $(".city-name").html(
+            "<h3>" + currentConditions.name + " " + "(" + moment().format("L") + ")" + "</h3>"
+        );
+    
+
+
+    }
+
+    function saveCityToSearchHistory(city) {
+
+        cityArr.push(city.toLowerCase());
+
+        localStorage.setItem("city", JSON.stringify(cityArr));
+    }
+
+    function getSearchHistory() {
+        var searchHistory = JSON.parse(localStorage.getItem("city"));
+
+        if (!searchHistory) {
+            return null;
+        }
+
+        cityArr = searchHistory;
+
+        $("#cities-list").empty();
+
+        for (var i = 0; i < searchHistory.length; i++) {
+            var cityButton = $("<button>");
+            cityButton.addClass("w-100 text-left city-btn");
+            cityButton.attr("data-name", searchHistory[i]);
+            cityButton.text(searchHistory[i]);
+
+            $("#cities-list").append(cityButton);
+        }
+    }
 
 
 
 
 
 
-})
+
+
+
+
+
+
+
+});
